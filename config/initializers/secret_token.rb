8d677356bb3c7050ae2e1056fd349decf9857894
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-11 09:49:40 +0100

    Generate new Rails app with Postgres, RSpec, Capybara and Haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpClone::Application.config.secret_key_base = 'e35ec0625cdc82fc8bf265db61ba958b712100d890fde982cff62e2168c3b33a99a26d3d88eb5b4300e3dc92db997cbb5feb5e9b7ba052798cabf9804e6b2140'
